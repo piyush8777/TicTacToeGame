f6c4033b500a81b306bcf975b430a8eddd4454c5
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-04 09:58:26 +0530

    ADD X or O

--- UC2AddXorO.java
+++ UC2AddXorO.java
@@ -1,4 +1,26 @@
 
 public class UC2AddXorO {
+	static void dispBoard() {
+		int count = 0;
+		for (int i = 0; i < BOARD.length; i++) {
+			System.out.println("---------------");
+			System.out.print("||");
+			for (int j = 0; j < BOARD[i].length; j++) {
+				if (BOARD[i][j] == 0) {
+					count++;
+					System.out.print(" o |");
+				} else if (BOARD[i][j] == 1) {
+					count++;
+					System.out.print(" x |");
+				} else
+					System.out.print("   |");
+			}
+			System.out.println("|");
+		}
+		if (count == 9) {
+			boolean isEmpty = false;
+		}
+		System.out.println("---------------");
 
+	}
 }
