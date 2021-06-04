d5eda4ce595fb45152f1d383120f610a2c63a847
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-04 08:55:58 +0530

    creating a board

--- BoardCreatingUC1.java
+++ BoardCreatingUC1.java
@@ -1,4 +1,17 @@
 
 public class BoardCreatingUC1 {
+	static int player=0;
+	static int[][] BOARD=new int[3][3];
+	static boolean isEmpty=true;
+	static void initBoard() {
+		System.out.println("TicTacToeGame");
+		for(int i=0;i<BOARD.length;i++) {
+			for(int j=0;j<BOARD[i].length;j++) {
+				BOARD[i][j]=10;
+			}
+		}
+		System.out.println("Board is this");
+		initBoard();
+	}
 
 }
